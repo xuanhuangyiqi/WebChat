f8875e95cf3d970f52a92435ce092f6513f3625c
htedsv <xuanhuangyiqi@126.com>
2013-05-01 23:07:27 +0800

    history

--- views.py
+++ views.py
@@ -34,17 +34,23 @@ class LogoutHandler(tornado.web.RequestHandler):
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
     clients = set()
+    messages = []
 
     def open(self):
         nick = self.get_cookie("nickname")
         for x in SocketHandler.clients:
             x.write_message('%s 来了'%nick)
         self.write_message('你好, %s！在线的人有%s'%(nick, ','.join([x.get_cookie("nickname") for x in SocketHandler.clients])))
+        for x in SocketHandler.messages:
+            self.write_message(x)
+
         SocketHandler.clients.add(self)
 
     def on_message(self, message):
+        m = "%s:%s<br />"%(self.get_cookie("nickname").decode('utf-8'), message)
+        SocketHandler.messages.append(m)
+        SocketHandler.messages = SocketHandler.messages[-20:]
         for x in SocketHandler.clients:
-            m = "%s:%s<br />"%(self.get_cookie("nickname").decode('utf-8'), message)
             x.write_message(m)
 
     def on_close(self):
